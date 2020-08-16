9fc0755f6d6f581bcb7a6d95001b9f9c5818a543
Sanaz Mahmoudi <mahmoudi.sanaz59@gmail.com>
2020-08-16 22:57:09 +0430

    Updates

---
+++ precache-manifest.fa5381e2c367cddb5db8ddbfec9ec9a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "395b37af564a2f3b6a1294c6d46db4f5",
+    "url": "/My-Portfolio-Website/index.html"
+  },
+  {
+    "revision": "a6c3749b3ca674bd116a",
+    "url": "/My-Portfolio-Website/static/css/main.c2820718.chunk.css"
+  },
+  {
+    "revision": "ea655f06279d3a911121",
+    "url": "/My-Portfolio-Website/static/js/2.89191288.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/My-Portfolio-Website/static/js/2.89191288.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6c3749b3ca674bd116a",
+    "url": "/My-Portfolio-Website/static/js/main.7367e33a.chunk.js"
+  },
+  {
+    "revision": "6a2fa2b71e9b572217b4",
+    "url": "/My-Portfolio-Website/static/js/runtime-main.4653eb5f.js"
+  }
+]);
