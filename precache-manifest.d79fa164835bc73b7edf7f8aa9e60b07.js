c60667c3f3c91d342f9aa18fce97a4f69fd631d3
Sanaz Mahmoudi <mahmoudi.sanaz59@gmail.com>
2020-08-21 21:25:56 +0430

    Updates

---
+++ precache-manifest.d79fa164835bc73b7edf7f8aa9e60b07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fca2a7b4ceca44ce6189b0ce8ae2432d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7c0637de65b17c052f85",
+    "url": "./static/css/main.1c36677b.chunk.css"
+  },
+  {
+    "revision": "9b6c69bd7d98b9fab9e8",
+    "url": "./static/js/2.b4222879.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b4222879.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c0637de65b17c052f85",
+    "url": "./static/js/main.2593fdf1.chunk.js"
+  },
+  {
+    "revision": "610a38b7b5a7cb7b2d5e",
+    "url": "./static/js/runtime-main.916853f0.js"
+  }
+]);
