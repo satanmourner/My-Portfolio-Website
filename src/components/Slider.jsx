81d88cb1ac06367119566601d62403e24384fd08
Sanaz Mahmoudi <mahmoudi.sanaz59@gmail.com>
2020-08-23 01:14:41 +0430

    click for slideshow ready

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,38 +1,34 @@
 import React, { Component } from 'react';
 import './section-slider.css';
 
+var slideIndex = 1;
+
 export default class Slider extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {show: [], clicked: false};
-    this.print = this.print.bind(this);
+    this.state = {show: []};
+    this.clickOnSlide = this.clickOnSlide.bind(this);
     this.refSlide = [];
   }
 
-  print(event) {
-    // for(let i = 0; i < slide.length; i++) {
-    //   (slide[i].is) ? slide[i].is = false : slide[i].is = true;
-    //   this.setState({show: this.state.show.concat(this.slide)});
-    // }
-
+  clickOnSlide = (event) => {
     for(let i = 0; i < slide.length; i++) {
+      slide[i].dis = false;
       if(event.target === this.refSlide[i]) {
-        
+       slide[i].dis = true;
+       this.setState({show: this.state.show.concat(slide[i].dis)})
       }
     }
-
-    console.log(event.target, this.refSlide[0]);
-    
   }
 
   render() {
     return (
       <div className="slider">
-        <img className="slide-img" src={process.env.PUBLIC_URL + '/Images/wall.png'}  /> 
+        <img className="slide-img" src={process.env.PUBLIC_URL + '/Images/wall.png'} /> 
           <div className="slides">
           {slide.map((slide, index) => (
             <div className="container-slide" key={slide.id}
-            style={{display: slide.is ? "flex" : "none"}}>
+            style={{display: slide.dis ? "flex" : "none"}}>
               {slide.h1}
               <a href={slide.href} target="_blank">
                 <button className="btn-slide">{slide.content} <i className={slide.i} /></button>
@@ -40,8 +36,8 @@ export default class Slider extends React.Component {
             </div>
         ))}
           <div className="dot-slide">
-                  <i className="fa fa-circle" onClick={this.print} ref={ref => this.refSlide.push(ref)} />
-                  <i className="fa fa-heart" onClick={this.print} ref={ref => this.refSlide.push(ref)} />
+            <i className="fa fa-circle" onClick={this.clickOnSlide} ref={ref => this.refSlide.push(ref)} />
+            <i className="fa fa-circle" onClick={this.clickOnSlide} ref={ref => this.refSlide.push(ref)} />
           </div>
         </div>
       </div>
@@ -51,16 +47,16 @@ export default class Slider extends React.Component {
 
 const slide = [
   {
-    id: 1,
-    is: true,
+    id: 0,
+    dis: true,
     h1: <h1>Hi! <br /> i'm Sanaz</h1>, 
     href: "https://docs.google.com/document/d/1sI9rfUIGP4uDuwhj3bFge--Iq4OdHMtgD4lJgey2-BU/edit?usp=sharing",
     content: "viwe cv",
     i: "far fa-file-pdf"
   },
   {
-    id: 2,
-    is: false,
+    id: 1,
+    dis: false,
     h1: <h1>i love building THINGS!!</h1>,
     href: "https://gitlab.com/mahmoudi.sanaz59",
     content: "my projects",
